6f1ce5d428bf9a73f2297b9153c0e5ba47c1e639
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2021-11-13 00:40:50 +0300

    Added new migrations and corrected old

--- migrations/01_itcode.up.sql
+++ migrations/01_itcode.up.sql
@@ -1,15 +1,3 @@
-drop table promo_condition_item;
-
-drop table promo_exclusions;
-
-drop table promo_gift_items;
-
-drop table promo_item_selector;
-
-drop table items;
-
-drop table promos;
-
 create table items
 (
     id    int unsigned auto_increment
@@ -76,5 +64,41 @@ create table promo_item_selector
     constraint promo_item_selector_FK
         foreign key (item_id) references items (id),
     constraint promo_item_selector_FK_1
+        foreign key (promo_id) references promos (id)
+);
+
+create table orders
+(
+    id        int unsigned auto_increment
+        primary key,
+    cart_id   varchar(10)  not null,
+    promocode varchar(100) not null
+);
+
+
+create table cart_items
+(
+    id      int unsigned auto_increment
+        primary key,
+    price   float        null,
+    item_id int unsigned not null,
+    cart_id int unsigned not null,
+    constraint cart_items_items_id_fk
+        foreign key (item_id) references items (id)
+            on update cascade on delete cascade,
+    constraint cart_items_orders_id_fk
+        foreign key (cart_id) references orders (id)
+            on update cascade on delete cascade
+);
+
+create table cart_promos
+(
+    id       int unsigned auto_increment
+        primary key,
+    promo_id int unsigned not null,
+    cart_id  int unsigned not null,
+    constraint cart_promos_orders_id_fk
+        foreign key (cart_id) references orders (id),
+    constraint cart_promos_promos_id_fk
         foreign key (promo_id) references promos (id)
 );
