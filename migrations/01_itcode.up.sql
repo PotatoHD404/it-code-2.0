62170acd10b42964d3e73bcf968daa3a85fcf00c
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2021-11-14 02:17:53 +0300

    Change pipeline

--- migrations/01_itcode.up.sql
+++ migrations/01_itcode.up.sql
@@ -1,14 +1,14 @@
-drop table items
+drop table items;
 
-drop table promos
+drop table promos;
 
-drop table promo_condition_item
+drop table promo_condition_item;
 
-drop table promo_exclusions
+drop table promo_exclusions;
 
-drop table promo_gift_items
+drop table promo_gift_items;
 
-drop table promo_item_selector
+drop table promo_item_selector;
 
 create table items
 (
